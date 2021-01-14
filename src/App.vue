9f5feaadb32b917408a1d53a1c39e284badbc920
jonnystv <jonathan@darefoundation.com>
2021-01-14 11:18:59 +0000

    Initial Commit - initial app setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data(){
+    return {
+      energyUse: []
+    }
+  },
+  mounted(){
+    fetch("https://api.carbonintensity.org.uk/generation")
+    .then(res => res.json())
+    .then(energyUseAPI => this.energyUse = energyUseAPI)
   }
-}
+
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
